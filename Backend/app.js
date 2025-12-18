0c8de522e764296f5f11444a9a329cf5e9514f34
Ryann <154708766+Ryannds-dev@users.noreply.github.com>
2025-12-18 23:34:55 +0100

    Initialize MongoDB Atlas connection, dotenv config and gitignore for .env in backend folder

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,16 @@ const express = require("express");
 const books = require("./books.json");
 const path = require("path");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.error("Connexion à MongoDB échouée :", err));
 
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "images")));
